a1cf22d8612b0c0fc79603fd2d0709265ffeb8ba
FreekAlberti <federicoalberi96@gmail.com>
2020-09-04 16:35:29 +0200

    alert

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@
 //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
 $(document).ready(function() {
-
+  var list = [];
+  for (var i = 0; i < 5; i++) {
+    var numero = numRandom();
+    list.push(numero);
+  }
+  alert(list);
 });
+
+function numRandom(){
+  return Math.floor(Math.random()* 100);
+};
