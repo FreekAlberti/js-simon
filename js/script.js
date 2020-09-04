43ca389cbae1af8492aacd7685c31f78a227439d
FreekAlberti <federicoalberi96@gmail.com>
2020-09-04 18:20:12 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -3,19 +3,22 @@
 //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
 $(document).ready(function() {
-  myAlert = numAlert();
-  setTimeout(invisible, 5000);
+  listaComputer = listaNumComputer();
   setTimeout(function() {
-    var numeriDaConfrontare = myPrompt();
-    console.log(numeriDaConfrontare);
-  }, 5100);
+    invisible();
+    var listaUtente = listaNumUtente();
+    var comuni = numComuni(listaUtente, listaComputer);
+    alert("Hai indovinato " + comuni.length + " numeri: " + comuni);
+  }, 5000);
 });
+
+//FUNZIONI
 
 function numRandom(){
   return Math.floor(Math.random()* 100);
 }
 
-function numAlert(){
+function listaNumComputer(){
   var list = [];
   for (var i = 0; i < 5; i++) {
     var numero = numRandom();
@@ -29,11 +32,31 @@ function invisible() {
   $(".visibile").addClass("invisible");
 }
 
-function myPrompt() {
+function listaNumUtente() {
   var listaUtente = [];
   for (var i = 0; i < 5; i++) {
     var numUtente = parseInt(prompt("scrivi uno dei numeri che hai letto"));
     listaUtente.push(numUtente);
   }
   return listaUtente;
+}
+
+function numComuni(listA, listB) {
+  var listASistemata = listA.concat().sort();
+  var listBSistemata = listB.concat().sort();
+  var comuni = [];
+  var ai = 0;
+  var bi = 0;
+  while (ai < listA.length && bi < listB.length) {
+    if (listASistemata[ai] === listBSistemata[bi]) {
+      comuni.push(listASistemata[ai]);
+      ai++;
+      bi++;
+    } else if(listASistemata[ai] < listBSistemata[bi]) {
+      ai++;
+    } else {
+      bi++;
+    }
+  }
+  return comuni;
 }
