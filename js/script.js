c7421d95ec2d93ceca79f553d599bc8bd013cba6
FreekAlberti <federicoalberi96@gmail.com>
2020-09-04 17:42:20 +0200

    lista computer lista utente

--- js/script.js
+++ js/script.js
@@ -3,14 +3,37 @@
 //Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
 $(document).ready(function() {
+  myAlert = numAlert();
+  setTimeout(invisible, 5000);
+  setTimeout(function() {
+    var numeriDaConfrontare = myPrompt();
+    console.log(numeriDaConfrontare);
+  }, 5100);
+});
+
+function numRandom(){
+  return Math.floor(Math.random()* 100);
+}
+
+function numAlert(){
   var list = [];
   for (var i = 0; i < 5; i++) {
     var numero = numRandom();
     list.push(numero);
   }
-  alert(list);
-});
+  $(".visibile").text(list);
+  return list;
+}
 
-function numRandom(){
-  return Math.floor(Math.random()* 100);
-};
+function invisible() {
+  $(".visibile").addClass("invisible");
+}
+
+function myPrompt() {
+  var listaUtente = [];
+  for (var i = 0; i < 5; i++) {
+    var numUtente = parseInt(prompt("scrivi uno dei numeri che hai letto"));
+    listaUtente.push(numUtente);
+  }
+  return listaUtente;
+}
